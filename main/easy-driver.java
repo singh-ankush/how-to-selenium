ada41e333a2389c1198e77a1fc41a375a9b3d23b
singh-ankush <102654406+singh-ankush@users.noreply.github.com>
2023-12-24 23:02:22 +0530

    Create easy-driver.java

---
+++ main/easy-driver.java
@@ -0,0 +1,31 @@
+// this is a simple example of how to open a website in selenium without requiring to install drivers or additional files.
+//this dependency is based on one provided by bonigarcia and i have just implemented it.
+
+import io.github.bonigarcia.wdm.WebDriverManager; //you will require this library for the execution(I will provide the maven dependencies at the end for this library.)
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Ankush Singh
+ */
+public class fixed_to_static {
+    public static void main(String[] args) {
+        String a = "//website you wish to open";
+        
+        WebDriverManager.chromedriver().setup();
+        
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get(a);
+        
+    }
+}
+
+
+//maven dependency
+// <dependency>
+//             <groupId>io.github.bonigarcia</groupId>
+//             <artifactId>webdrivermanager</artifactId>
+//             <version>5.5.2</version>
+//         </dependency>
