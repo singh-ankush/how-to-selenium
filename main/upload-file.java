dc3fb74e428752e41fd226c287a38d6e4f0ce1f2
singh-ankush <102654406+singh-ankush@users.noreply.github.com>
2023-12-25 19:50:19 +0530

    Update upload-file.java

--- main/upload-file.java
+++ main/upload-file.java
@@ -1,3 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager; //you can find the implementation of this in my easy-driver.java file.
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Ankush Singh
+ */
+public class fixed_to_static {
+    public static void main(String[] args) {
+
 WebElement file = driver.findElement(By.xpath("//path to your web element"));
 
 file.sendKeys("//location of your file for rg:C:\\Computer\\file.txt");
+
+      driver.close();
+
+    }
+}
+
