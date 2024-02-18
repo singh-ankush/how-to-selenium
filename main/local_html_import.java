c98852787b6cc8a64f4d33db56a4654e251abd4c
Ankush Kumar Singh <102654406+singh-ankush@users.noreply.github.com>
2024-02-18 12:10:00 +0530

    Update local_html_import.java

--- main/local_html_import.java
+++ main/local_html_import.java
@@ -1,1 +1,15 @@
+// this is a code to open an html file you have created and is locally available on your system by giving file location.
+
+public class local_html_import {
+    public static void main(String[] args) {
+        File f = new File("C://Downloads//file.html"); //location to your html file
+        
+        WebDriverManager.chromedriver().setup();
+        
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get(f.toString());
+        
+    }
+}
 
